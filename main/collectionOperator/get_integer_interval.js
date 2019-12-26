67848c1fcf2db0f918653d0943d83ffd1fcef63c
lpdft <772346730@qq.com>
2019-12-26 21:15:39 +0800

    [ruyue] feat: collectionOperator\collection-calculate-camp

--- main/collectionOperator/get_integer_interval.js
+++ main/collectionOperator/get_integer_interval.js
@@ -2,6 +2,16 @@
 
 function get_integer_interval(number_a, number_b) {
   //在这里写入代码
+  var result = [];
+  var min, max;
+  (number_a <= number_b) ? (min = number_a, max = number_b) : (min = number_b, max = number_a);
+  for(var i = min; i <= max; i++) {
+    result.push(i);
+  }
+  if(number_a > number_b) {
+    result.reverse();
+  }
+  return result;
 }
 
 module.exports = get_integer_interval;
