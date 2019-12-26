d23ece7dc42eabc22e84d0f7ef7bcf4822998af8
lpdft <772346730@qq.com>
2019-12-26 21:13:31 +0800

    [ruyue] feat: collectionOperator\get_integer_interval_2

--- main/collectionOperator/get_integer_interval_2.js
+++ main/collectionOperator/get_integer_interval_2.js
@@ -2,6 +2,19 @@
 
 function get_integer_interval_2(number_a, number_b) {
   //在这里写入代码
+  var result = [];
+  var min, max;
+  (number_a <= number_b) ? (min = number_a, max = number_b) : (min = number_b, max = number_a);
+  for(var i = min; i <= max; i++) {
+    if(!(i % 2)) {
+      result.push(i);
+    }
+  }
+  if(number_a > number_b) {
+    result.reverse();
+  }
+  return result;
 }
+
 
 module.exports = get_integer_interval_2;
