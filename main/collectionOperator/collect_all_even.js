d383b2588313d1cbaffb8fabdbb6f1d70acaec46
lpdft <772346730@qq.com>
2019-12-26 21:07:50 +0800

    [ruyue] feat: collectionOperator\collect_all_even_spec.js

--- main/collectionOperator/collect_all_even.js
+++ main/collectionOperator/collect_all_even.js
@@ -2,6 +2,13 @@
 
 function collect_all_even(collection) {
   //在这里写入代码
+  var result = [];
+  collection.forEach(function(value) {
+    if(!(value % 2)) {
+      result.push(value);
+    }
+  });
+  return result;
 }
 
 module.exports = collect_all_even;
