3c1a5e2378002f1d4e1fe4956e68a8a652f36d0a
lpdft <772346730@qq.com>
2019-12-26 21:17:02 +0800

    [ruyue] feat: collectionOperator\collection-calculate-camp

--- main/collectionOperator/get_intersection.js
+++ main/collectionOperator/get_intersection.js
@@ -2,6 +2,13 @@
 
 function get_intersection(collection_a, collection_b) {
   //在这里写入代码
+  var data = [];
+  collection_b.forEach(function(value) {
+    if(collection_a.indexOf(value) !== -1) {
+      data.push(value);
+    }
+  });
+  return data;
 }
 
 module.exports = get_intersection;
