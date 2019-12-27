f688467fa746e2909100bbc5bff328ba8b0e74f4
lpdft <772346730@qq.com>
2019-12-27 16:22:01 +0800

    [ruyue] feat: reduce\reduce\compare_collections_spec.js

--- main/reduce/compare_collections.js
+++ main/reduce/compare_collections.js
@@ -2,6 +2,10 @@
 
 function compare_collections(collection_a, collection_b) {
   //在这里写入代码
+  var isEqual = collection_a.reduce(function(total,currentValue,currentIndex) {
+    return total && (currentValue === collection_b[currentIndex])
+  },true);
+  return isEqual;
 }
 
 module.exports = compare_collections;
