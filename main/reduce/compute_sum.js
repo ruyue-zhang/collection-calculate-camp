e7618cadc65fe7c7b7d09acce20eddd02d5eed08
lpdft <772346730@qq.com>
2019-12-27 17:10:25 +0800

    [ruyue] feat: reduce\compute_sum_spec

--- main/reduce/compute_sum.js
+++ main/reduce/compute_sum.js
@@ -2,6 +2,10 @@
 
 function calculate_elements_sum(collection) {
   //在这里写入代码
+  var sum = collection.reduce(function(x,y) {
+    return x + y;
+  },0);
+  return sum;
 }
 
 module.exports = calculate_elements_sum;
