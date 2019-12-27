3dc8b7a19c4456f1f846403e335fcc19d97ae353
lpdft <772346730@qq.com>
2019-12-27 17:07:36 +0800

    [ruyue] feat: reduce\compute_median_spec

--- main/reduce/compute_median.js
+++ main/reduce/compute_median.js
@@ -2,6 +2,23 @@
 
 function compute_median(collection) {
   //在这里写入代码
+  arraySortAsc(collection);
+  var median = getMedianNumber(collection);
+  return median;
+}
+
+
+
+function arraySortAsc(dataArray) {
+  dataArray.sort(function(a,b) {
+    return a - b;
+  });
+}
+
+function getMedianNumber(dataArray) {
+  var len = dataArray.length;
+  var median = len % 2 ? dataArray[Math.floor(len / 2)] : (dataArray[len / 2] + dataArray[len / 2 - 1]) / 2;
+  return median;
 }
 
 module.exports = compute_median;
