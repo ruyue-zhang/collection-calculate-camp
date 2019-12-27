6748b9be108d109afea4222f52b774256f005734
lpdft <772346730@qq.com>
2019-12-27 15:41:52 +0800

    [ruyue] feat: reduce\collect_max_number_spec

--- main/reduce/collect_max_number.js
+++ main/reduce/collect_max_number.js
@@ -2,6 +2,10 @@
 
 function collect_max_number(collection) {
   //在这里写入代码
+  var max =collection.reduce(function(x,y) {
+    return (x > y) ? x : y;
+  });
+  return max;
 }
 
 module.exports = collect_max_number;
