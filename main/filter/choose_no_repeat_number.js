3c067f0824875c3075c34485e1d47d68100ee871
lpdft <772346730@qq.com>
2019-12-27 10:42:12 +0800

    [ruyue] feat: filter\choose_no_repeat_number

--- main/filter/choose_no_repeat_number.js
+++ main/filter/choose_no_repeat_number.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function choose_no_repeat_number(collection) {
-
   //在这里写入代码
+  var result = [];
+  collection.forEach(function(value) {
+    if(-1 === result.indexOf(value)) {
+      result.push(value);
+    }
+  });
+  return result;
 }
 
 module.exports = choose_no_repeat_number;
