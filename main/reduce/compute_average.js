3fe8d520c1be9ecd4f0f20321b89d159ea8fcf11
lpdft <772346730@qq.com>
2019-12-27 16:26:43 +0800

    [ruyue] feat: reduce\reduce\compute_average_spec

--- main/reduce/compute_average.js
+++ main/reduce/compute_average.js
@@ -2,6 +2,10 @@
 
 function compute_average(collection) {
   //在这里写入代码
+  var totalValue = collection.reduce(function(total,currentValue) {
+    return total + currentValue;
+  },0);
+  return totalValue / collection.length;
 }
 
 module.exports = compute_average;
