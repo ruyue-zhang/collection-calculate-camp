a342e83d202c0e21ce53daa463e5036ccda719a4
lpdft <772346730@qq.com>
2019-12-27 13:04:51 +0800

    [ruyue] feat: flaten/map/number_map_to_word_over_26_spec

--- main/map/number_map_to_word_over_26.js
+++ main/map/number_map_to_word_over_26.js
@@ -1,6 +1,17 @@
 'use strict';
 var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+  var alphabetArray = ['0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s',
+                       't','u','v','w','x','y','z'];
+  var result = collection.map(function(value) {
+    if(value <= 26 ) {
+      return alphabetArray[value];
+    }
+    else {
+      var count = Math.ceil(value / 26) - 1;
+      return alphabetArray[count] + alphabetArray[value - 26 * count];
+    }
+  });
+  return result;
 };
 
 module.exports = number_map_to_word_over_26;
