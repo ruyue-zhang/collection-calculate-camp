b06224e7b5b134de75b196a736f014cfc28776d8
lpdft <772346730@qq.com>
2019-12-27 10:38:17 +0800

    [ruyue] feat: filter\choose_multiples_of_three

--- main/filter/choose_multiples_of_three.js
+++ main/filter/choose_multiples_of_three.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function choose_multiples_of_three(collection) {
-
   //在这里写入代码
+  var result = [];
+  collection.forEach(function(value) {
+    if(!(value % 3)) {
+      result.push(value);
+    }
+  });
+  return result;
 }
 
 module.exports = choose_multiples_of_three;
