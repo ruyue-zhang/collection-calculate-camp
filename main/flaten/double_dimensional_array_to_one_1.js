163713ca2d7160c4a245c9c152b6dc241c5f6362
lpdft <772346730@qq.com>
2019-12-27 12:48:42 +0800

    [ruyue] feat: flaten/double_dimensional_array_to_one_1

--- main/flaten/double_dimensional_array_to_one_1.js
+++ main/flaten/double_dimensional_array_to_one_1.js
@@ -1,8 +1,19 @@
 'use strict';
 
 function double_to_one(collection) {
-
   //在这里写入代码
+  var result = [];
+  collection.forEach(function(value1) {
+    if(value1 instanceof Array) {
+      value1.forEach(function(value2) {
+        result.push(value2);
+      });
+    }
+    else {
+      result.push(value1);
+    }
+  });
+  return result;
 }
 
 module.exports = double_to_one;
