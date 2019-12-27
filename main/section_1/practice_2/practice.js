29330f71a1a373c42ee6503446f772b5c5f17377
lpdft <772346730@qq.com>
2019-12-27 18:49:48 +0800

    [ruyue] feat: section_1\practice_2_spec.js

--- main/section_1/practice_2/practice.js
+++ main/section_1/practice_2/practice.js
@@ -1,5 +1,14 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  var result = [];
+  collection_a.forEach(function(value_a) {
+    collection_b.forEach(function(value_b) {
+      if(-1 !== value_b.indexOf(value_a)) {
+        result.push(value_a);
+      }
+    });
+  });
+  return result;
 }
 
 module.exports = collect_same_elements;
