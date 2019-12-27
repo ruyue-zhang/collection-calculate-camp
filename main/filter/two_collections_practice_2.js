9b824a12a710b7f8b2bec2579b2ca89acd93bb93
lpdft <772346730@qq.com>
2019-12-27 12:09:16 +0800

    [ruyue] feat: filter\choose_no_common_elements

--- main/filter/two_collections_practice_2.js
+++ main/filter/two_collections_practice_2.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function choose_no_common_elements(collection_a, collection_b) {
-
   //在这里写入代码
+  var result = [];
+  collection_a.forEach(function(value) {
+    if(-1 === collection_b.indexOf(value)) {
+      result.push(value);
+    }
+  });
+  return result;
 }
 
 module.exports = choose_no_common_elements;
