6051e4187e629a49e6c9145cb495b550dd82bb5d
lpdft <772346730@qq.com>
2019-12-27 20:57:00 +0800

    [ruyue] feat: section_1\practice_3_spec.js

--- main/section_1/practice_3/practice.js
+++ main/section_1/practice_3/practice.js
@@ -1,5 +1,12 @@
 function collect_same_elements(collection_a, object_b) {
   //在这里写入代码
+  var result = [];
+  collection_a.forEach(function(value) {
+      if(-1 !== object_b.value.indexOf(value)) {
+        result.push(value);
+      }
+  });
+  return result;
 }
 
 module.exports = collect_same_elements;
