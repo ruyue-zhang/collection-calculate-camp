8f896c30f53656d702faefbd10070d9add121d1a
lpdft <772346730@qq.com>
2019-12-27 10:36:38 +0800

    [ruyue] feat: filter\choose_even_spec

--- main/filter/choose_even.js
+++ main/filter/choose_even.js
@@ -1,8 +1,15 @@
 'use strict';
 
 function choose_even(collection) {
-
   //在这里写入代码
+  var result = [];
+  collection.forEach(function(value) {
+    if(!(value % 2)) {
+      result.push(value);
+    }
+  });
+  return result;
+
 }
 
 module.exports = choose_even;
