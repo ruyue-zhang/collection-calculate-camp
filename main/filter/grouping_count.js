ece58b64a2dc5417907b6f08940730ce03d1c11f
lpdft <772346730@qq.com>
2019-12-27 12:04:09 +0800

    [ruyue] feat: filter\grouping_count

--- main/filter/grouping_count.js
+++ main/filter/grouping_count.js
@@ -1,8 +1,12 @@
 'use strict';
 
 function grouping_count(collection) {
-
   //在这里写入代码
+  var result = {};
+  for (var i = 0; i < collection.length; i++) {
+    collection[i] in result ? result[collection[i]]++ : result[collection[i]]=1;
+  }
+  return result;
 }
 
 module.exports = grouping_count;
