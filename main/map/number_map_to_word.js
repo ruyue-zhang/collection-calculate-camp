28960ee0301611306279c44e5e52ce3f23e1605f
lpdft <772346730@qq.com>
2019-12-27 13:07:18 +0800

    [ruyue] feat: flaten/map/number_map_to_word_spec.js

--- main/map/number_map_to_word.js
+++ main/map/number_map_to_word.js
@@ -1,6 +1,11 @@
 'use strict';
 var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+  var alphabetArray = ['0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s',
+                       't','u','v','w','x','y','z'];
+  var result = collection.map(function(value) {
+    return alphabetArray[value];
+  });
+  return result;
 };
 
 module.exports = number_map_to_word;
