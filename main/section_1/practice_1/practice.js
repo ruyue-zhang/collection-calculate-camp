02e8a5129f55bc7cfcf0e6f7e9b62de773463f19
lpdft <772346730@qq.com>
2019-12-27 18:23:15 +0800

    [ruyue] feat: section_1\practice_1_spec

--- main/section_1/practice_1/practice.js
+++ main/section_1/practice_1/practice.js
@@ -1,5 +1,12 @@
 function collect_same_elements(collection_a, collection_b) {
   //在这里写入代码
+  var result = [];
+  collection_a.forEach(function(value) {
+    if(-1 !== collection_b.indexOf(value)) {
+      result.push(value);
+    }
+  });
+  return result;
 }
 
 module.exports = collect_same_elements;
