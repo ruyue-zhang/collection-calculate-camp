17aa0c66e686bd0e45fadc9af3fadc36528ea9ca
lpdft <772346730@qq.com>
2019-12-27 12:15:34 +0800

    [ruyue] feat: filter\choose_divisible_integer

--- main/filter/two_collections_practice_3.js
+++ main/filter/two_collections_practice_3.js
@@ -1,8 +1,16 @@
 'use strict';
 
 function choose_divisible_integer(collection_a, collection_b) {
-
   //在这里写入代码
+  var result = [];
+  collection_a.forEach(function(value_a) {
+    collection_b.forEach(function(value_b) {
+      if(value_a % value_b === 0) {
+        result.push(value_a);
+      }
+    });  
+  });
+  return result;
 }
 
 module.exports = choose_divisible_integer;
