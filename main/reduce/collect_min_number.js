3d97f00597ebcfcf035c086f1edd4bf8e1a89494
lpdft <772346730@qq.com>
2019-12-27 15:42:45 +0800

    [ruyue] feat: reduce\collect_min_number_spec

--- main/reduce/collect_min_number.js
+++ main/reduce/collect_min_number.js
@@ -2,6 +2,10 @@
 
 function collect_min_number(collection) {
   //在这里写入代码
+  var max =collection.reduce(function(x,y) {
+    return (x < y) ? x : y;
+  });
+  return max;
 }
 
 module.exports = collect_min_number;
