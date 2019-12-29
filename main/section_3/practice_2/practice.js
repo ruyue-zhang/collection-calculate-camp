f37269d349268ce4e4b9b7c686c7b6bf5f73e805
lpdft <772346730@qq.com>
2019-12-29 18:26:18 +0800

    [ruyue] feat: \section_3\practice_2_spec.js

--- main/section_3/practice_2/practice.js
+++ main/section_3/practice_2/practice.js
@@ -1,5 +1,14 @@
 function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
+  object_b.value.forEach(function(value_b) {
+    collection_a.forEach(function(value_a) {
+      if(value_a.key === value_b) {
+        var cutNumber = parseInt(value_a.count / 3);
+        value_a.count -= cutNumber;
+      }
+    });
+  });
+  return collection_a;
 }
 
 module.exports = create_updated_collection;
