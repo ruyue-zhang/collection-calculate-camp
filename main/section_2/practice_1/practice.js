e4b1ff52dd331de61999e93f65849641894efecb
lpdft <772346730@qq.com>
2019-12-29 16:36:34 +0800

    [ruyue] feat: \section_2\practice_1_spec

--- main/section_2/practice_1/practice.js
+++ main/section_2/practice_1/practice.js
@@ -1,5 +1,19 @@
 function count_same_elements(collection) {
   //在这里写入代码
+  var result = [];
+  collection.forEach(function(valueCollect) {
+    var isExist = false;
+    result.forEach(function(valueResult) {
+      if(valueCollect === valueResult.key) {
+        valueResult.count++;
+        isExist = true;
+      }
+    });
+    if(!isExist) {
+      result.push({key: valueCollect, count: 1});
+    }
+  });
+  return result;
 }
 
 module.exports = count_same_elements;
