1fa3380606806d553a0ba2c985669a0b774a4ac6
lpdft <772346730@qq.com>
2019-12-29 17:20:03 +0800

    [ruyue] feat: \section_2\practice_2_spec

--- main/section_2/practice_2/practice.js
+++ main/section_2/practice_2/practice.js
@@ -1,5 +1,45 @@
 function count_same_elements(collection) {
   //在这里写入代码
+  var result = [];
+  collection.forEach(function(valueCollect) {
+    if(valueCollect.length <= 1) {
+      countSingleLetter(valueCollect,result);
+    }
+    else {
+      countStringLetter(valueCollect,result);
+    }
+  });
+  return result;
 }
+
+
+function countSingleLetter(valueCollect,result) {
+  var isExist = false;
+  result.forEach(function(valueResult) {
+    if(valueCollect === valueResult.key) {
+      valueResult.count++;
+      isExist = true;
+    }
+  });
+  if(!isExist) {
+    result.push({key: valueCollect, count: 1});
+  }
+}
+
+function countStringLetter(valueCollect,result) {
+  var isExist = false;
+  result.forEach(function(valueResult) {
+    if(valueCollect.substring(0,1) === valueResult.key) {
+      valueResult.count += Number(valueCollect.substring(2));
+      isExist = true;
+    }
+  });
+  if(!isExist) {
+    var keyValue = valueCollect.substring(0,1);
+    var countValue = Number(valueCollect.substring(2));
+    result.push({key: keyValue, count: countValue});
+  }
+}
+
 
 module.exports = count_same_elements;
