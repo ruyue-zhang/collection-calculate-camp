193a8ddc51a228893559f2bd4c45adf4f543139f
lpdft <772346730@qq.com>
2019-12-29 20:59:17 +0800

    [ruyue] feat: superposition_operation\collection_operation\letter_mapping_1_spec

--- main/superposition_operation/collection_operation/letter_mapping_1.js
+++ main/superposition_operation/collection_operation/letter_mapping_1.js
@@ -1,8 +1,25 @@
 'use strict';
 
 function even_to_letter(collection) {
-
   //在这里写入代码
+  var alphabetArray = getAlphabetArray();
+  return getLetterByEven(alphabetArray,collection);
+}
+
+function getAlphabetArray() {
+  var alphabetArray = ['0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s',
+                       't','u','v','w','x','y','z'];
+  return alphabetArray;
+}
+
+function getLetterByEven(alphabetArray,collection) {
+  var result = [];
+  collection.forEach(function(value) {
+    if(!(value % 2)) {
+      result.push(alphabetArray[value]);
+    }
+  });
+  return result;
 }
 
 module.exports = even_to_letter;
