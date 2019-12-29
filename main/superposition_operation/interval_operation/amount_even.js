2a6c97b0875eb26494758b3ea666032e3850461d
lpdft <772346730@qq.com>
2019-12-29 21:37:50 +0800

    [ruyue] feat: superposition_operation\interval_operation\average_uneven_spec

--- main/superposition_operation/interval_operation/amount_even.js
+++ main/superposition_operation/interval_operation/amount_even.js
@@ -2,10 +2,9 @@
 
 function amount_even(collection) {
   //在这里写入代码
-  var result = collection.reduce(function(total,currentValue) {
+  var sum = collection.reduce(function(total,currentValue) {
     return currentValue % 2 ? total : total + currentValue;
   },0);
-  return result;
 }
 
 module.exports = amount_even;
--- main/superposition_operation/interval_operation/average_uneven.js
+++ main/superposition_operation/interval_operation/average_uneven.js
@@ -1,8 +1,16 @@
 'use strict';
 
 function average_uneven(collection) {
-
   //在这里写入代码
+  var total = 0;
+  var count = 0;
+  collection.forEach(function(value) {
+    if(value % 2) {
+      count++;
+      total += value;
+    }
+  });
+  return total / count;
 }
 
 module.exports = average_uneven;
