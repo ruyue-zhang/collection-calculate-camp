84d3eed20e55a08f2e60528ce3d59e5a5819d9bc
lpdft <772346730@qq.com>
2019-12-29 21:12:27 +0800

    [ruyue] feat: superposition_operation\collection_operation\practice_1_spec.js

--- main/superposition_operation/collection_operation/practice_1.js
+++ main/superposition_operation/collection_operation/practice_1.js
@@ -1,8 +1,11 @@
 'use strict';
 
 function hybrid_operation(collection) {
-
   //在这里写入代码
+  var sum = collection.reduce(function(total,currentValue) {
+    return total + currentValue * 3 + 2;
+  },0);
+  return sum;
 }
 
 module.exports = hybrid_operation;
