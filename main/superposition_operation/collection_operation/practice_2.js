e5af58ca2be7e98c169c0203823b4f97b1e4ff48
lpdft <772346730@qq.com>
2019-12-29 21:18:52 +0800

    [ruyue] feat: superposition_operation\collection_operation\practice_2_spec.js

--- main/superposition_operation/collection_operation/practice_2.js
+++ main/superposition_operation/collection_operation/practice_2.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
-
   //在这里写入代码
+  var result = [];
+  collection.forEach(function(value) {
+    if(value % 2) {
+      result.push(value * 3 + 2);
+    }
+  });
+  return result;
 }
 
 module.exports = hybrid_operation_to_uneven;
