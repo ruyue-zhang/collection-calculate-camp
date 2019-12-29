3a7ce891394283045019cc491df6b6db61950755
lpdft <772346730@qq.com>
2019-12-29 21:22:30 +0800

    [ruyue] feat: superposition_operation\collection_operation\practice_3_spec.js

--- main/superposition_operation/collection_operation/practice_3.js
+++ main/superposition_operation/collection_operation/practice_3.js
@@ -1,8 +1,16 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
-
   //在这里写入代码
+  var result = collection.reduce(function(total,currentValue) {
+    if(currentValue % 2) {
+      return total + currentValue * 3 + 5;
+    }
+    else {
+      return total;
+    }
+  },0);
+  return result;
 }
 
 module.exports = hybrid_operation_to_uneven;
