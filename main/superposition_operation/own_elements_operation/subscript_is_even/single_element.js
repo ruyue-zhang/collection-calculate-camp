caa4d187c086198bffc3b0e42124e79878d7f7f4
lpdft <772346730@qq.com>
2019-12-30 14:57:34 +0800

    [ruyue] feat: superposition_operation\own_elements_operation\subscript_is_even\single_element_spec

--- main/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,5 +1,41 @@
 'use strict';
 var single_element = function(collection){
-
+  var evenElement = getEvenElement(collection);
+  var noRepeatElement = getNoRepeatElementArray(evenElement);
+  return noRepeatElement;
 };
+
+function getEvenElement(collection) {
+  var evenElement = [];
+  for(var i = 1; i < collection.length; i += 2) {
+    evenElement.push(collection[i]);
+  }
+  return evenElement;
+}
+
+function getNoRepeatElementArray(evenElement) {
+  var noRepeatElement = [];
+  var len = evenElement.length;
+  for(var i = 0; i < len; i++) {
+    var isExist = false;
+    for(var j = 0; j < len; j++) {
+      if(i === j) {
+        continue;
+      }
+      else {
+        if(evenElement[i] === evenElement[j]) {
+          isExist = true;
+          break;
+        }
+      } 
+    }
+    if(!isExist) {
+      noRepeatElement.push(evenElement[i]);
+    }
+  }
+  return noRepeatElement;
+}
+
+
+
 module.exports = single_element;
