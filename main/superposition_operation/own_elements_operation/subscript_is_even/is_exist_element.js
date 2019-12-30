f29656fd06cad40e340987a2be844320efde6e4d
lpdft <772346730@qq.com>
2019-12-30 13:34:19 +0800

    [ruyue] feat: superposition_operation\own_elements_operation\subscript_is_even\is_exist_element_spec

--- main/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,5 +1,24 @@
 'use strict';
 var is_exist_element = function(collection,element){
+  var indexIsEvevArray = getIndexEven(collection);
+  return isExist(indexIsEvevArray,element);
+}
 
-};
+function getIndexEven(collection) {
+  var indexIsEvevArray = [];
+  for(var i = 0; i < collection.length; i += 2) {
+    indexIsEvevArray.push(collection[i]);
+  }
+  return indexIsEvevArray;
+}
+
+function isExist(indexIsEvevArray,element) {
+  var isExist = false;
+  indexIsEvevArray.forEach(function(value) {
+    if(element === value) {
+      isExist = true;
+    }
+  });
+  return isExist;
+}
 module.exports = is_exist_element;
