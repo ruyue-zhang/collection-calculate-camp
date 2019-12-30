d1ea082cf716d83c3faf94a5d042048fef353c98
lpdft <772346730@qq.com>
2019-12-30 17:29:32 +0800

    [ruyue] feat: superposition_operation\own_elements_operation\rank_by_two_large_one_small_spec

--- main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ main/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,5 +1,36 @@
 'use strict';
 function rank_by_two_large_one_small(collection){
   //这里写代码。。。
+  //先按从小到大排序
+  sortAsc(collection);
+  return sortByTwoLargeOneSmall(collection);
+
 }
+function sortAsc(collection) {
+  collection.sort(function(a,b) {
+    return a - b;
+  });
+}
+
+function sortByTwoLargeOneSmall(collection) {
+  var result = [];
+  var len = collection.length;
+  for(var i = 0; i < len; i += 3) {
+    if(i + 2 < len) {
+      result.push(collection[i + 1]);
+      result.push(collection[i + 2]);
+      result.push(collection[i]);
+    }
+    else if(i + 1 < len){
+      result.push(collection[i]);
+      result.push(collection[i + 1]);
+    }
+    else {
+      result.push(collection[i]);
+    } 
+  }
+  return result;
+}
+
+
 module.exports = rank_by_two_large_one_small;
