68281ab4ca96266c52df8b5745fc1862b6ef85d0
lpdft <772346730@qq.com>
2019-12-30 13:27:24 +0800

    [ruyue] feat: superposition_operation\own_elements_operation\subscript_is_even\even_group_calculate_average_spec

--- main/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
@@ -1,5 +1,63 @@
 'use strict';
 var even_group_calculate_average = function(collection){
-
+  var evenArray = getEvenNumberArray(collection);
+  var valueIsEven = getAllValueIsEven(evenArray);
+  if(valueIsEven.length) {
+    var result = getSortAverage(valueIsEven);
+    return result;
+  }
+  else {
+   return [0];
+  } 
 };
+
+function getEvenNumberArray(collection) {
+  var evenArray = [];
+  for(var i = 1; i < collection.length; i += 2) {
+    evenArray.push(collection[i]);
+  }
+  return evenArray;
+}
+
+function getAllValueIsEven(evenArray) {
+  var reuslt = [];
+  evenArray.forEach(function(value) {
+    if(!(value % 2)) {
+      reuslt.push(value);
+    }
+  });
+  return reuslt;
+}
+
+function getSortAverage(valueIsEven) {
+  var sum1 = 0, count1 = 0;
+  var sum2 = 0, count2 = 0;
+  var sum3 = 0, count3 = 0;
+  var result = [];
+  valueIsEven.forEach(function(value) {
+    if(value < 10) {
+      sum1 += value;
+      count1++;
+    }
+    else if(value < 100) {
+      sum2 += value;
+      count2++;
+    }
+    else {
+      sum3 += value;
+      count3++;
+    }
+  });
+  if(count1) {
+    result.push(sum1 / count1);
+  }
+  if(count2) {
+    result.push(sum2 / count2);
+  }
+  if(count3) {
+    result.push(sum3 / count3);
+  }
+  return result;
+}
+
 module.exports = even_group_calculate_average;
