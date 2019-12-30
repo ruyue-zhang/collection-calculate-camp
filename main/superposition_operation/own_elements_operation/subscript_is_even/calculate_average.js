b65632b587c3f807ac3572c3e8bd63b0ba48b9f4
lpdft <772346730@qq.com>
2019-12-30 11:13:28 +0800

    [ruyue] feat: superposition_operation\own_elements_operation\subscript_is_even\calculate_average_spec

--- main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
+++ main/superposition_operation/own_elements_operation/subscript_is_even/calculate_average.js
@@ -1,5 +1,11 @@
 'use strict';
 var calculate_average = function(collection){
-
+  var count = 0;
+  var sum = 0;
+  for(var i = 2; i < collection.length; i += 2) {
+    count++;
+    sum += collection[i];
+  }
+  return sum / count;
 };
 module.exports = calculate_average;
