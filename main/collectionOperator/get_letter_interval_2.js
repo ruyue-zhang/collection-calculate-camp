6c5ef7940b8f7cee264793b829f891235d59524d
lpdft <772346730@qq.com>
2020-01-03 10:18:46 +0800

    [ruyue] fixed: \collectionOperator\get_letter_interval_2—_2_spec-fromCharCode() function

--- main/collectionOperator/get_letter_interval_2.js
+++ main/collectionOperator/get_letter_interval_2.js
@@ -1,36 +1,27 @@
 'use strict';
 
 function get_letter_interval_2(number_a, number_b) {
-  //在这里写入代码
-  var alphabetArray = getAlphabetArray();
-  return getLetterByInputNumber(alphabetArray,number_a, number_b);
+  if(number_a < number_b) {
+    return getLetterByInputNumber(number_a, number_b);
+  }
+  else {
+    return getLetterByInputNumber(number_b, number_a).reverse();
+  }
 }
 
-function getAlphabetArray() {
-  var alphabetArray = ['0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s',
-                       't','u','v','w','x','y','z'];
-  return alphabetArray;
-}
-
-function getLetterByInputNumber(alphabetArray,number_a, number_b) {
+function getLetterByInputNumber(min, max) {
   var result = [];
-  var min, max;
-  (number_a <= number_b) ? (min = number_a, max = number_b) : (min = number_b, max = number_a);
   for(var i = min; i <= max; i++) {
     if(i <= 26 ) {
-      result.push(alphabetArray[i]);
+      result.push(String.fromCharCode(i + 96));
     }
     else {
       var count = Math.ceil(i / 26) - 1;
-      result.push(alphabetArray[count] + alphabetArray[i - 26 * count]);
+      result.push(String.fromCharCode(count + 96) + String.fromCharCode(i - 26 * count + 96));
     }
-  }
-  if(number_a > number_b) {
-    result.reverse();
   }
   return result;
 }
-
 
 module.exports = get_letter_interval_2;
 
