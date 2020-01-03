e62a546c40c37332bbf2d0c6dfda75c9521b4c11
lpdft <772346730@qq.com>
2020-01-03 10:05:56 +0800

    [ruyue] fixed: \collectionOperator\get_integer_interval_spec

--- main/collectionOperator/get_integer_interval.js
+++ main/collectionOperator/get_integer_interval.js
@@ -4,7 +4,15 @@ function get_integer_interval(number_a, number_b) {
   //在这里写入代码
   var result = [];
   var min, max;
-  (number_a <= number_b) ? (min = number_a, max = number_b) : (min = number_b, max = number_a);
+  if(number_a < number_b) {
+    min = number_a;
+    max = number_b;
+  }
+  else {
+    min = number_b;
+    max = number_a;
+  }
+  
   for(var i = min; i <= max; i++) {
     result.push(i);
   }
