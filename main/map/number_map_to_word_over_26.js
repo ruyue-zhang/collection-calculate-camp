5d7dfeb8c0e40566e89956e14414910e4b3b6b4c
lpdft <772346730@qq.com>
2020-01-03 10:32:59 +0800

    [ruyue] fixed: \map\-fixed return to inline

--- main/map/number_map_to_word_over_26.js
+++ main/map/number_map_to_word_over_26.js
@@ -1,17 +1,15 @@
 'use strict';
+
 var number_map_to_word_over_26 = function(collection){
-  var alphabetArray = ['0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s',
-                       't','u','v','w','x','y','z'];
-  var result = collection.map(function(value) {
+  return collection.map(function(value) {
     if(value <= 26 ) {
-      return alphabetArray[value];
+      return String.fromCharCode(value + 96);
     }
     else {
       var count = Math.ceil(value / 26) - 1;
-      return alphabetArray[count] + alphabetArray[value - 26 * count];
+      return String.fromCharCode(count + 96) + String.fromCharCode(value - 26 * count + 96);
     }
   });
-  return result;
 };
 
 module.exports = number_map_to_word_over_26;
